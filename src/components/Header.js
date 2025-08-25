d7821de13145949007c4ba907b2ce6c0bb009314
Ana St.Aubyn <anacstaubyn@gmail.com>
2025-08-25 10:26:40 +0100

    change header font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
       <header className="hero">
-        <h1 className="logo">T & A</h1>
-        <p>Our Wedding — September 12, 2026</p>
+        <h1 className="logo">Ana & Tiago</h1>
+        <p>12 de Setembro de 2026</p>
       </header>
     );
   }
